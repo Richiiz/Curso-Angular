8031a707d73354c54c237e7adbcb401b5c319a83
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-24 12:07:57 -0500

    String interpolation la forma en la que desde la logica, pasamos datos a renderizar a nuestro template, la vista

--- my_store/src/app/app.component.ts
+++ my_store/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my_store';
+  name = 'Richiiz';
+  age = 19;
+  img = 'https://yt3.ggpht.com/K9zEiBjtQowbvmr5yEb7VDU2FNDQd07Kc3X0wXxaigP9ibKDlMFHtIYOuA9zDRKgLy5bGWhJ=s900-c-k-c0x00ffffff-no-rj';
 }
