e82e6f819bb6243f1a3ec0b7be9386595469ca77
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-24 16:52:50 -0500

    uso de ngFor

--- my_store/src/app/app.component.ts
+++ my_store/src/app/app.component.ts
@@ -16,6 +16,12 @@ export class AppComponent {
     age: 34,
     avatar: 'https://yt3.ggpht.com/K9zEiBjtQowbvmr5yEb7VDU2FNDQd07Kc3X0wXxaigP9ibKDlMFHtIYOuA9zDRKgLy5bGWhJ=s900-c-k-c0x00ffffff-no-rj'
   }
+
+  names: string[] = ['nico', 'juli', 'elfa'];
+  NewName  = '';
+  NewLista: string[] = ['papel', 'pasra', 'papitas'];
+  lista = '';
+
   toggleButton(){
     this.btnDisable = !this.btnDisable;
   }
@@ -29,5 +35,20 @@ export class AppComponent {
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+  AddName(){
+    this.names.push(this.NewName);
+    // imput limpiado despues de agregar nuevo nombre
+    this.NewName = '';
+  }
+  deleteName(index: number){
+    this.names.splice(index, 1);
+  }
+  anadir(){
+    this.NewLista.push(this.lista);
+    this.NewLista
+  }
+  borrar(index: number){
+    this.NewLista.splice(index, 1)
   }
 }
