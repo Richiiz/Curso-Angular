bc4c444b3ee728d8940158a31232539f93e476a7
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-24 12:26:48 -0500

    Aprendiendo que es Property Binding, como y cuando se usa :D

--- my_store/src/app/app.component.ts
+++ my_store/src/app/app.component.ts
@@ -9,4 +9,11 @@ export class AppComponent {
   name = 'Richiiz';
   age = 19;
   img = 'https://yt3.ggpht.com/K9zEiBjtQowbvmr5yEb7VDU2FNDQd07Kc3X0wXxaigP9ibKDlMFHtIYOuA9zDRKgLy5bGWhJ=s900-c-k-c0x00ffffff-no-rj';
+  btnDisable= true;
+
+  person = {
+    name: 'nicolas el vergas',
+    age: 34,
+    avatar: 'https://yt3.ggpht.com/K9zEiBjtQowbvmr5yEb7VDU2FNDQd07Kc3X0wXxaigP9ibKDlMFHtIYOuA9zDRKgLy5bGWhJ=s900-c-k-c0x00ffffff-no-rj'
+  }
 }
