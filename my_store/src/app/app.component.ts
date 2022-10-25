91ce74c997f9b057604ebc9b8f7e49f155256a8e
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-25 10:34:21 -0500

    class an style usando class binding

--- my_store/src/app/app.component.ts
+++ my_store/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+widthImg = 10
+
   name = 'Richiiz';
   age = 19;
   img = 'https://yt3.ggpht.com/K9zEiBjtQowbvmr5yEb7VDU2FNDQd07Kc3X0wXxaigP9ibKDlMFHtIYOuA9zDRKgLy5bGWhJ=s900-c-k-c0x00ffffff-no-rj';
